3a8bbbe72fa48051a36a07a6c936fe94b9a3334f
sheebu2004 <156988925+sheebu2004@users.noreply.github.com>
2026-04-05 06:19:02 +0530

    Added contructor to UserController.

--- friendship_App/Controllers/UsersController.cs
+++ friendship_App/Controllers/UsersController.cs
@@ -7,5 +7,9 @@ namespace friendship_App.Controllers
     [Route("api/[controller]")]
     public class UsersController
     {
+        public UsersController()
+        {
+            
+        }
     }
 }
